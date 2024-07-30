1554d6f674423a0d51fc0cca3b07e1e403fe37a6
smadiyar000 <sholpansmd@gmail.com>
2024-07-29 19:52:11 -0500

    resource creation

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "name" {
+  type = string
+  default = ""
+  description = "Please replace default value with your domain name" 
+}
+
+variable "type" {
+    type = string
+    default = ""
+    description = "Please replace with your record type"
+}
+
+variable "env" {
+    type = string
+    default = ""
+    description = "Please replace with your subdomain env name"
+}
+
